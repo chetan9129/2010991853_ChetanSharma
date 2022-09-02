a02d8a5bba74f80dd3c14b78db0fff94e6ab1585
Chetan Sharma <anku9129@gmail.com>
2022-09-02 13:33:28 +0530

    Props 02 Sept 2022

---
+++ 13. React Work/react-task-tracker/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./Component/Header.js";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello</h2>
+      <Header title='React Tutorial'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 13. React Work/react-task-tracker/src/Component/Header.js
@@ -0,0 +1,9 @@
+const Header = (props) => {
+  return (
+    <header>
+      <h1>Hello {props.title}</h1>
+    </header>
+  );
+};
+
+export default Header;
